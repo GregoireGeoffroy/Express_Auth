b7a5b5c85c9c946089bbb6cdbe7e575d1a16a248
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-26 11:22:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+        const user = rows[0];
+  
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        if (user.password !== password) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch(err) {
+        return done(err);
+      }
+    })
+  );
+  
+
 
 app.get("/sign-up", (req, res) => {
     console.log("Sign-up route accessed");
