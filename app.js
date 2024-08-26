591d5c44b63229111c7f818338a5f6d8dac1c4da
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-26 11:27:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -78,6 +78,14 @@ app.post("/sign-up", async (req, res, next) => {
   app.get("/", (req, res) => {
     res.render("index", { user: req.user });
   });
-    
+  app.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+  
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
