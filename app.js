69551afae3b003a302bf03cd936f542364d24fb4
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-26 11:21:39 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+const pool = new Pool({
+  // add your configuration
+});
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get("/sign-up", (req, res) => {
+    console.log("Sign-up route accessed");
+    res.render("sign-up-form");
+  });
+  
+
+app.post("/sign-up", async (req, res, next) => {
+    try {
+      await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+        req.body.username,
+        req.body.password,
+      ]);
+      res.redirect("/");
+    } catch(err) {
+      return next(err);
+    }
+  });
+app.get("/", (req, res) => res.render("index"));
+  
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
